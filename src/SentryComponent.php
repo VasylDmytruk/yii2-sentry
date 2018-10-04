34998d1477354d7d4d8d8b4de98d318d7d5d0056
Vasyl DMytruk <vasyl.dmytruk@xloo.com>
2018-10-04 17:00:53 +0300

    First commit.

---
+++ src/SentryComponent.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace autoxloo\yii2\sentry;
+
+/**
+ * Class SentryComponent
+ */
+class SentryComponent
+{
+    public static function test()
+    {
+        echo __METHOD__ . PHP_EOL;
+    }
+}
